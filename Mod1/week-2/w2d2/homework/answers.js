5529c56b097fb3d105326ae493977d35166b5255
sfwinters <sarah.f.winters@gmail.com>
2020-06-05 12:25:17 -0400

    Fizz Buzz answered

--- Mod1/week-2/w2d2/homework/answers.js
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -14,4 +14,19 @@ for (i = 0; i <= 200; i++) {
     if (i % 2 === 0) {
         console.log(i);
     }
-}
+};
+
+//Fizz Buzz
+for (i = 1; i <=100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+};
+
+//Wild Wild Life
