5e782040a2ec308a46e13634fc774e0a88400814
sfwinters <sarah.f.winters@gmail.com>
2020-06-13 17:02:02 -0400

    uploading lab

--- Mod1/week-3/w3d3/student_examples/derp.js
+++ Mod1/week-3/w3d3/student_examples/derp.js
@@ -1,26 +1,6 @@
-const adventurer = {
-    name: 'Timothy',
-    hitpoints: 10,
-    belongings: ['sword', 'potion', 'Tums'],
-    companion: {
-        name: 'Velma',
-        type: 'bat'
-    }
-}
-
-console.log(adventurer.companion.name) //Velma
-
-adventurer.companion.name = 'Susan'
-
-console.log(adventurer.companion.name) //Susan
-
-adventurer.companion2 = {
-    name: 'Dingus',
-    type: 'insect'
-}
-// console.log(adventurer.companion2)
-
-adventurer.companion.companion = {
-    name: 'Buttmunch',
-    type: 'butt parasite'
-}
+//Hungry For More?
+//1
+const solarArray = Object.keys(system);
+// //2
+const stuff = solarArray.sort()
+console.log(stuff.sort('velocity'))
