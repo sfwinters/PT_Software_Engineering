b111817aabe23286d79887fb7cf80a23fd5611e8
sfwinters <sarah.f.winters@gmail.com>
2020-06-19 18:38:47 -0400

    combine objects, arrays, and functions more than one level deep

--- Mod1/week-4/w4d2/homework/combining_datatypes.js
+++ Mod1/week-4/w4d2/homework/combining_datatypes.js
@@ -29,3 +29,37 @@ const apartmentBuilding = [
     floorplans = ['1 bedroom', '2 bedroom', '3 bedroom']
 ]
 console.log(apartmentBuilding[0][1]) //Dingus McCringus
+
+//More Than One Level Deep
+
+//1
+function knit(item, size, color, yarn) {
+    this.item = item;
+    this.size = size;
+    this.color = color;
+    this.yarn = yarn;
+}
+const birthdayGift = new knit('sweater', 'large', 'green', 'itchy');
+console.log(birthdayGift.yarn);
+
+//2
+function crayonSelector(color, needSharpened, label) {
+    this.color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    this.needSharpened = needSharpened;
+    this.label = label
+}
+const whichCrayon = new crayonSelector('red', true, 'ripped');
+console.log(whichCrayon.color[4]) //blue
+
+//3
+const worldsLamestJukebox = ['Rico Suave', 'Whoomp (There It Is)', 'Electric Avenue', 'The Curly Shuffle'];
+function options(songNum) {
+    if (songNum in worldsLamestJukebox) {
+        console.log('Pick a song: ' + worldsLamestJukebox[songNum])
+    }
+}
+
+function powerButton(num) {
+    num = options(num);
+}
+powerButton(2)
