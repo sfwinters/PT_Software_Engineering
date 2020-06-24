3d9e89ddf98cb93ef82434f9cca2f26a0848c606
sfwinters <sarah.f.winters@gmail.com>
2020-06-24 12:14:47 -0400

    created person class

--- Mod1/week-5/w5d1/homework/objects.js
+++ Mod1/week-5/w5d1/homework/objects.js
@@ -11,6 +11,49 @@ class Hamster {
         console.log('nibble nibble')
     }
     getPrice() {
-        return this.price
+        return this.price;
+    }
+}
+
+class Person {
+    constructor(name) {
+        this.name = name,
+        this.age = 0,
+        this.height = 0,
+        this.weight = 0,
+        this.mood = 0,
+        this.hamsters = [],
+        this.bankAccount = 0
+    }
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    greet() {
+        console.log('Hello, ' + name);
+    }
+    eat() {
+        weight += 1;
+        mood += 1;
+    }
+    exercise() {
+        weight -=1;
+    }
+    ageUp() {
+        age +=1;
+        height +=1;
+        weight +=1;
+        mood -=1;
+        this.bankAccount +=10;
+    }
+    buyHamster() {
+        this.hamsters.push(new Hamster('Gravy'));
+        mood +=1;
+        this.bankAccount -= Hamster.getPrice()
     }
 }
