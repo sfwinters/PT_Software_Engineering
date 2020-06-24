42ac909cc937d7393f12f266c4e5c0df50b6afc5
sfwinters <sarah.f.winters@gmail.com>
2020-06-24 12:55:39 -0400

    created Pizza Rat

--- Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
+++ Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
@@ -24,3 +24,29 @@ class Hero {
 }
 const dougie = new Hero('Dougie the Doughnut');
 console.log(dougie)
+
+class Enemy {
+    constructor(name) {
+        this.name = name,
+        this.health = 100,
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        },
+        this.catchPhrases = [
+            'I\'m YouTube famous!', 'I\'m more dangerous than an uncovered sewer.'
+        ]
+    }
+    talkSmack() {
+        smack = Math.floor(Math.random());
+        console.log(this.catchPhrases[smack]);
+    }
+    announceHealth() {
+        console.log(this.health)
+    }
+    fight() {
+        console.log('I\'m gonna flatten you like a slice of pepperoni!')
+    }
+}
+const pizzaRat = new Enemy('Pizza Rat')
+console.log(pizzaRat)
