6f1d0efa9e1faa1f174dd1ea020fc15a225d828c
sfwinters <sarah.f.winters@gmail.com>
2020-06-28 10:56:18 -0400

    I added my first element and it was 'fun'

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -1,3 +1,11 @@
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
+// if (typeof jQuery == 'undefined'){
+//     console.log('oops! I still have to link my jQuery properly!');
+//   } else {console.log('I did it! I linked jQuery and this js file!')};
+
+$(() => {
+    let $container = document.getElementById('container')
+    // console.log($container)
+    let $h1 = $('<h1></h1>').text('Hogwarts');
+    $h1.appendTo($container);
+    console.log($h1)
+});
