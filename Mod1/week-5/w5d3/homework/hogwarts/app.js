455748f2d47df3a9dcde1c87ee957743488d2b9b
sfwinters <sarah.f.winters@gmail.com>
2020-06-28 11:23:11 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -6,6 +6,12 @@ $(() => {
     let $container = document.getElementById('container')
     // console.log($container)
     let $h1 = $('<h1></h1>').text('Hogwarts');
-    $h1.appendTo($container);
-    console.log($h1)
+    let $name = $('<h2></h2>').text('Sarah Winters');
+    let $house = $('<h3></h3>').text('Ravenclaw');
+    let $pet = $('<h4></h4>').text('Goblin Face');
+    $pet.addClass('cat');
+    let $wand = $('<h4></h4>').text('Hornbeam Wand with Dragon Heartstring Core');
+    $($container).append($h1, $name, $house, $pet, $wand)
+    // console.log($pet)
+    document.getElementsByClassName('cat')
 });
