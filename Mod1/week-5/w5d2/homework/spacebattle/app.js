beb65aebcd59cb85c864d608a68a3180a3819d27
sfwinters <sarah.f.winters@gmail.com>
2020-06-28 00:42:15 -0400

    fight mechanics almost done

--- Mod1/week-5/w5d2/homework/spacebattle/app.js
+++ Mod1/week-5/w5d2/homework/spacebattle/app.js
@@ -12,10 +12,17 @@ class Spaceship {
             this.hull = Math.floor((Math.random() * 3) + 3);
             this.firepower = Math.floor((Math.random() * 2) + 2);
             this.accuracy = ((Math.random() * 0.2) + 0.6).toFixed(2);
-            alienFleet.push(this.name);
+            alienFleet.push(this);
         }
     }
-}
+    hullStatus() {
+        console.log(`${this.name}'s hull is currently at ${this.hull}.`)
+    };
+    fireOn(rival) {
+        this.accuracy = Math.random();
+        rival.accuracy = Math.random();
+        }
+    }
 const arnie = new Spaceship('USS Schwarzenegger', 'Earth')
 const alien1 = new Spaceship('001', 'Aliens');
 const alien2 = new Spaceship('002', 'Aliens');
@@ -24,3 +31,45 @@ const alien4 = new Spaceship('004', 'Aliens');
 const alien5 = new Spaceship('005', 'Aliens');
 const alien6 = new Spaceship('006', 'Aliens');
 
+console.log(alienFleet)
+
+spaceBattle = () => {
+    for (i = 0; i < 6; i++) {
+        enemy = alienFleet[i]; 
+        while (arnie.hull > 0 && enemy.hull > 0) {
+            arnie.fireOn(enemy);
+            if (arnie.accuracy >= enemy.accuracy) {
+                enemy.hull -= arnie.firepower;
+                console.log(`Direct hit! ${enemy.name} took damage.`)
+                enemy.hullStatus();
+                if (alienFleet[5].hull <= 0) {
+                    console.log(`Well done, soldier! You have defeated the alien menace. YOU WIN`);
+                    return;
+                } else if (enemy.hull <= 0) {
+                    // let choice;
+                    // let yes = confirm(`${alienFleet[i].name} has been destroyed! Do you want to retreat or continue?`);
+                    // if (yes == true) {
+                    //     choice = "Let's do this!";
+                    //     arnie.fight(alienFleet[i + 1])
+                    // } else {
+                    //      choice = `You have retreated to fight another day.`;
+                    // }
+                } else {
+                    console.log(`The shot missed!`);
+                }; 
+            };
+            enemy.fireOn(arnie);
+                if (enemy.accuracy >= arnie.accuracy && enemy.firepower > 0) {
+                arnie.hull -= enemy.firepower;
+                console.log(`Direct hit! ${arnie.name} took damage.`)
+                arnie.hullStatus();
+                if (arnie.hull <= 0) {
+                    console.log(`Your ship was destroyed! YOU LOSE`);
+                    return;
+                }
+            }
+        }
+    }
+}
+            
+spaceBattle();
--- Mod1/week-5/w5d2/homework/spacebattle/derp.js
+++ Mod1/week-5/w5d2/homework/spacebattle/derp.js
@@ -16,5 +16,24 @@ if alien ship is destroyed
         if retreat, game over
 
 */
+announceHealth() {
+    console.log(`${this.name}'s current health level is ${this.health}.`)
+};
+fight(rival) {
+    const dougieItems = Object.keys(dougie.weapons);
+    let hp = Object.values(dougie.weapons)
+    const hit = Math.floor(Math.random() * 2)
+    const bam = dougieItems[hit]
+    const pow = hp[hit]
+    rival.health -= pow
+    console.log('I\'m ready to rumble!');
+    console.log(`Dougie used ${bam}!`)
+    console.log(`${rival.name} was hit by ${bam}! His health is now at ${rival.health}!`)
 
-console.log(Math.random())
+
+    // const ask = prompt(`${alienFleet[i].name} has been destroyed! Do you want to retreat or continue?`);
+                    // if (ask != null) {
+                    //     arnie.fight(alienFleet[i + 1])
+                    // } else {
+                    //     console.log(`You have retreated to fight another day.`)
+                }
