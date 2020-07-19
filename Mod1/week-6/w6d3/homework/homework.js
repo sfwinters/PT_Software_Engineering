7c21dccef3b462cf839b603603684e6fa6d15f14
sfwinters <sarah.f.winters@gmail.com>
2020-07-19 19:38:32 -0400

    w6d3 homework done

--- Mod1/week-6/w6d3/homework/homework.js
+++ Mod1/week-6/w6d3/homework/homework.js
@@ -2,25 +2,31 @@ const $colorPalette = $('#color-palette');
 const $myPalette = $('#my-palette');
 const $generate = $('#generate');
 
+
+
+        
 const makePalette = () => {
+    const addColor = () => {
+        const hue = $(event.currentTarget).css('background-color');
+        $square = $('<div>');
+        $square.addClass('square');
+        $square.css('background-color', hue);
+        $myPalette.append($square);
+    };
 	for(let i = 0; i < 150; i++){
+        $square = $('<div>')
+        $square.addClass('square');
         const red = Math.floor(Math.random() * 255);
         const green = Math.floor(Math.random() * 255);
         const blue = Math.floor(Math.random() * 255);
         const color = 'rgb('+red+','+green+','+blue+')';
-		const $square = $('<div>');
-		$square.addClass('square');
         $square.css('background-color', color);
         $colorPalette.append($square);
-	}
-}
+    } 
+    $('.square').on('click', addColor);
+};
 
 
+$generate.on('click', makePalette);
 
-// $generate.on('click', makePalette);
-
-
-
-
-//... bottom of file
 makePalette();
