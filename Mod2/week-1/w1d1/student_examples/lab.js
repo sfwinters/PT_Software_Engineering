9c06c4c49bad196a685633c83c93894e9a3389c9
sfwinters <sarah.f.winters@gmail.com>
2020-07-23 11:55:42 -0400

    w9d1 lab

--- Mod2/week-1/w1d1/student_examples/lab.js
+++ Mod2/week-1/w1d1/student_examples/lab.js
@@ -1,11 +1,26 @@
-let hashmapTest = new Map();
+let sum = 0;
+let j;
+let wackyArray = [];
 
-const arr = [ [10, 20, 30],
+const numArray = [ [10, 20, 30],
             [40, 50, 60],
             [70, 80, 90] ];
 
-function horizontalSum(...arr) {
-    let total=0;
-    hashmapTest.push(total += arr)
-}
-console.log(horizontalSum(arr))
+numArray.map(function(row, i) {
+    let rows = numArray.map(function(row) { return row[i]; });
+    wackyArray.push(rows);
+});
+for (i = 0; i < numArray.length; i++) {
+    wackyArray.push(numArray[i].reduce(function(a, b) { return a + b }))
+};
+for (i = 0; i < wackyArray.length; i++) {
+    if (typeof wackyArray[i] === 'object') {
+        let erbject = wackyArray[i].reduce(function(a, b) { return (a + b )});
+        wackyArray.splice(i, 1, erbject)
+    }
+}   
+let biggest = wackyArray.reduce((a, b) => wackyArray[a] > wackyArray[b] ? a : b)
+
+
+console.log(wackyArray)
+console.log(biggest + ' is the largest sum in the array.')
