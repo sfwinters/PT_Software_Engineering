d45fb23d7fb0826c824ffea3d9649243edf1da5f
sfwinters <sarah.f.winters@gmail.com>
2020-07-23 15:39:29 -0400

    cleaned up code

--- Mod2/week-1/w1d1/student_examples/lab.js
+++ Mod2/week-1/w1d1/student_examples/lab.js
@@ -1,31 +1,25 @@
-let sum = 0;
-let j;
-let wackyArray = [];
-
 const numArray = [ [10, 20, 30],
             [40, 50, 60],
             [70, 80, 90] ];
 
-function bigAssFunction (arr) { 
-    arr.map(function(row, i) {
-        rows = arr.map(function(row) { return row[i]; });
-        wackyArray.push(rows);
-        });
-    for (i = 0; i < arr.length; i++) {
-        wackyArray.push(arr[i].reduce(function(a, b) { return a + b }))
-    };
-    for (i = 0; i < wackyArray.length; i++) {
-        if (typeof wackyArray[i] === 'object') {
-            let erbject = wackyArray[i].reduce(function(a, b) { return (a + b )});
-            wackyArray.splice(i, 1, erbject)
+let tempHolding = []
+
+const funkshun = arr => {
+    for (j = 0; j < arr.length; j++) {
+        let jVal = 0;
+        for (i = 0; i < arr.length; i++) {
+            jVal += arr[i][j];
+            let iVal = (arr[i].reduce(function(a, b) { return a + b }))
+            tempHolding.push(iVal)
         }
-    };   
-    let biggest = wackyArray.reduce((a, b) => wackyArray[a] > wackyArray[b] ? a : b);
-    console.log(wackyArray);
-    console.log(biggest + ' is the largest sum in the array.');
+        tempHolding.push(jVal);
+    }
+    let biggest = Math.max(...tempHolding);
+    console.log(tempHolding);
+    console.log(biggest + ' is the largest sum in this array.')
 }
 
-bigAssFunction(numArray)
+funkshun(numArray)
 
 const bigArray =
 [ [ 887, -541, -430, -590, 117, 172, -319, -18 ],
@@ -37,4 +31,5 @@ const bigArray =
   [ -345, 906, 83, 676, 723, 381, 557, -183 ],
   [ 199, -943, -710, 565, 193, 315, 281, 245 ] ];
 
-bigAssFunction(bigArray)
+  funkshun(bigArray)
+ console.log(tempHolding.sort())
