1664cd43272030f5886880578124553cc05e570c
sfwinters <sarah.f.winters@gmail.com>
2020-08-05 19:22:56 -0400

    fixed jquery code

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -3,39 +3,48 @@
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
 
 $(() => {
-    let $container = document.getElementById('container')
-    // console.log($container)
-    let $h1 = $('<h1></h1>').text('Hogwarts');
-    let $name = $('<h2></h2>').text('Sarah Winters');
-    let $house = $('<h3></h3>').text('Ravenclaw');
-    let $pet = $('<h4></h4>').text('Goblin Face');
+    let $container = $('#container')
+    console.log($container)
+    let $h1 = $('<h1>')
+    $h1.text('Hogwarts');
+    let $name = $('<h2>').text('Sarah Winters');
+    let $house = $('<h3>').text('Ravenclaw');
+    let $pet = $('<h4>').text('Goblin Face');
     $pet.addClass('cat');
-    let $wand = $('<h4></h4>').text('Hornbeam Wand with Dragon Heartstring Core');
-    $($container).append($h1, $name, $house, $pet, $wand)
-    // console.log($pet)
-    $($container).append('<ul id = \'trunk\'></ul>');
-    let $trunk = document.getElementById('trunk');
-    $trunk.setAttribute('storage', 'trunk');
-    $($trunk).append('<li>butter beer</li>', '<li class = "secret">invisibility cloak</li>', '<li class = "secret">magic map</li>', '<li class = "secret">time turner</li>', '<li class = "cat">leash</li>', '<li>Bertie Bott\'s Every Flavor Beans</li>')
-    // console.log($trunk)
-    $($container).append('<h5>Summer 2020</h5>');
+    let $wand = $('<h4>').text('Hornbeam Wand with Dragon Heartstring Core');
+    $container.append($h1, $name, $house, $pet, $wand)
+    console.log($pet)
+    $container.append('<ul id = \'trunk\'></ul>');
+    let $trunk = $('#trunk');
+    $trunk.attr('storage', 'trunk');
+    let $butterbeer = $('<li>').text('Butter Beer')
+    let $cloak = $('<li>').text('invisibility cloak').addClass('secret')
+    let $map = $('<li>').text('magic map').addClass('secret')
+    let $timeturner = $('<li>').text('time turner').addClass('secret')
+    let $leash = $('<li>').text('leash').addClass('cat')
+    let $beans = $('<li>').text('Bertie Bott\'s Every Flavor Beans')
+    $trunk.append($butterbeer, $cloak, $map, $timeturner, $leash, $beans)
+    console.log($trunk)
+    $container.append('<h5>Summer 2020</h5>');
     $wand.remove()
-    let $inventory = document.getElementsByTagName('li')
+    let $inventory = $('li')
+    console.log($inventory)
     $inventory[0].remove()
-    $($trunk).prepend('<li>Rosewood Wand with Phoenix Feather Core</li>')
-    $inventory[0].style.color = 'green'
-    $($pet).detach()
-    $($trunk).append($pet)
-    $($house).append($pet)
+    let $newwand = $('<li>').text('Rosewood Wand with Phoenix Feather Core')
+    $trunk.prepend($newwand)
+    $newwand.css('color', 'green')
+    $pet.detach()
+    $trunk.append($pet)
+    $house.append($pet)
     $('.secret').hide('slow')
     $('.secret').delay(2000).show('slow')
-    let $leash = $('.cat')[1]
-    // console.log($leash)
-    $($leash).addClass('cabbage')
-    $($leash).removeClass('cabbage')
-    let $h5 = document.getElementsByTagName('h5')
+    console.log($leash)
+    $leash.addClass('cabbage')
+    $leash.removeClass('cabbage')
+    let $h5 = $('h5')
+    console.log($h5)
     $h5[0].innerHTML = 'Fall 2020'
-    $($trunk).prepend('<li>butter beer</li>')
-    $trunk.setAttribute('storage', 'chest')
+    $trunk.prepend($butterbeer)
+    $trunk.attr('storage', 'chest')
     console.log($trunk)
 });
